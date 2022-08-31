0270a7ea60254ffcd7fd0491ac7a46fa6008ace4
Henok Belay <henokbelay.a@gmail.com>
2022-08-31 11:38:37 -0700

    commit

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
-
 def square_matrix_simple(matrix=[]):
-    return ([list(map(lambda x: x * x, row)) for row in matrix]) 
+    s = [[x**2 for x in i] for i in matrix]
+    return s 
