f1283435a38d3cd61273fedc03e02d06e1b088d7
Henok Belay <henokbelay.a@gmail.com>
2022-09-13 12:21:34 -0700

    commit

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,13 +1,11 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
-    c = y = 0
-    for i in my_list:
-        c += 1
+    c = 0
     for i in range(x):
         try:
-            print("{}".format(my_list[i]), end='')
-            y += 1
+            print(my_list[i], end="")
+            c += 1
         except IndexError:
             break
-    print("")
+    print()
     return 
